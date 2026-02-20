6acbc4e58d264eb98fb430e9ae734c5aa2eece5f
rygyfygy <237001230+rygyfygy@users.noreply.github.com>
2026-02-20 19:21:08 +0100

    Working game with event listeners.

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ function Player(name, marker) {
     return prompt(`${this.name}, please make your move: `);
   };
 
-  this.move = function () {
-    const dest = this.input();
-    board[dest[0]][dest[1]] = this.marker;
+  this.move = function (r,c) {
+    board[r][c] = this.marker;
   };
 }
 
@@ -28,35 +27,48 @@ let currentPlayer;
 
 const setCurrentPlayer = (players) => {
   if (currentPlayer) {
-    currentPlayer.marker === "o" ? currentPlayer = player1 : currentPlayer = player2;
-  }
-  else {
+    currentPlayer.marker === "o"
+      ? (currentPlayer = player1)
+      : (currentPlayer = player2);
+  } else {
     currentPlayer = players[Math.floor(Math.random() * 2)];
-  };
-}
-
-function checkWinner(board) {
+  }
+};
+const checkWinner = function(gameBoard) {
   const lines = [
     // rows
-    ...board,
+    ...gameBoard,
     // cols
-    ...[0, 1, 2].map((c) => board.map((row) => row[c])),
+    ...[0, 1, 2].map((c) => gameBoard.map((r) => r[c])),
     // diagonals
-    [board[0][0], board[1][1], board[2][2]],
-    [board[0][2], board[1][1], board[2][0]],
+    [gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]],
+    [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]],
   ];
 
-  for (const p of players) {
-    if (lines.some((line) => line.every((cell) => cell === p.marker))) {
-      alert(`${p.name} WINS!`);
-      return;
-    }
+  if (lines.some((line) => line.every((cell) => cell === currentPlayer.marker))) {
+    alert(`${currentPlayer.name} WINS!`);
+    return;
   }
 
   if (lines.every((line) => line.every((cell) => cell != null))) {
     alert("TIE!");
     return;
   }
+};
+
+
+setCurrentPlayer(players);
+for (let cell of document.querySelectorAll('.cell')) {
+  cell.addEventListener('click', (event) => {
+    const row = parseInt(event.target.id[0]);
+    const col = parseInt(event.target.id[1]);
+    if (board[row][col] === null) {
+      event.target.textContent = currentPlayer.marker;
+      currentPlayer.move(row,col);
+      checkWinner(board);
+      setCurrentPlayer(players);
+    }
+  });
 }
 
 // TODO: refractor logic, add event listeners
